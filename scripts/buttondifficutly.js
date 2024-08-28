a5ac90f13ea5dae87b5cf32c3480487f7e3f2d73
aaa-chinese <tiantiandd@outlook.com>
2024-08-28 13:16:10 +0800

    final menu (probably)

--- scripts/buttondifficutly.js
+++ scripts/buttondifficutly.js
@@ -1,14 +1,14 @@
 Events.on(EventType.ClientLoadEvent, () => {
-    Vars.ui.menufrag.addButton("CruxScape Settings", () => {
+    Vars.ui.menufrag.addButton("CruxScape\nSettings", () => {
         // let buf = Core.settings.getBool("cruxahh-diff");
         // buf=!buf;
         // Core.settings.put("cruxahh-diff", buf);
         let diffString="";
         Core.settings.getBool("cruxahh-diff")==0?diffString="NORMAL":diffString="HARD";
         const CSprompt = new BaseDialog("CruxScape Difficulty Setting");
-        CSprompt.cont.add("You can change the mod's difficutly settings here.").row();
-        CSprompt.cont.add("\"NORMAL\" means normal casual campaign difficutly, while \"HARD\" disables launch pad and wave simulation.").row();
-        CSprompt.cont.add("Current difficutly: "+diffString).row();
+        CSprompt.cont.add("You can change the mod's difficulty settings here.").row();
+        CSprompt.cont.add("\"NORMAL\" means normal casual campaign experience, while \"HARD\" disables launch pad and wave simulation.").row();
+        CSprompt.cont.add("Current difficulty: "+diffString).row();
         CSprompt.cont.button("NORMAL",()=>{
             Core.settings.put("cruxahh-diff",false);
             CSprompt.hide();
