f83fcae65241aa490ed39e5b231ae44f656fdddd
aaa-chinese <tiantiandd@outlook.com>
2024-08-28 12:40:14 +0800

    menu button..

--- scripts/buttondifficutly.js
+++ scripts/buttondifficutly.js
@@ -1,11 +1,6 @@
 Events.on(EventType.ClientLoadEvent, () => {
     Vars.ui.menufrag.addButton("stringTest", () => {
-        if(Core.settings.getInt("cruxahh-diff")==0)Core.settings.put("cruxahh-diff", 0xf0);
-        else{
-            let buf = Core.settings.getInt("cruxahh-diff");
-            buf++;
-            if(buf>0xf3)buf=0xf0;
-            Core.settings.put("cruxahh-diff", buf);
-        }
-    });
-});
+        let buf = Core.settings.getBool("cruxahh-diff");
+        buf=!buf
+        Core.settings.put("cruxahh-diff", buf);
+    })});
