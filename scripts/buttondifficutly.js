b4b5b07da1e07b717c32e48f7006fd44b264b7be
aaa-chinese <tiantiandd@outlook.com>
2024-08-28 12:35:40 +0800

    menu button went wrong

--- scripts/buttondifficutly.js
+++ scripts/buttondifficutly.js
@@ -1,11 +1,11 @@
 Events.on(EventType.ClientLoadEvent, () => {
     Vars.ui.menufrag.addButton("stringTest", () => {
-        if(Core.settings.getInt("cruxahh-diff")==0)Core.settings.put("cruxahh-diff", 100);
+        if(Core.settings.getInt("cruxahh-diff")==0)Core.settings.put("cruxahh-diff", 0xf0);
         else{
             let buf = Core.settings.getInt("cruxahh-diff");
             buf++;
-            if(buf>104)buf=100;
-            Core.settings.put("cruxahh-diff", 100);
+            if(buf>0xf3)buf=0xf0;
+            Core.settings.put("cruxahh-diff", buf);
         }
     });
 });
