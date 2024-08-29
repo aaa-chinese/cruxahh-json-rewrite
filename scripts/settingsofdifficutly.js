6c6edbb39add2f9b6fe75595485d525de727df57
aaa-chinese <tiantiandd@outlook.com>
2024-08-29 18:41:49 +0800

    fix logic reversed bug

--- scripts/settingsofdifficutly.js
+++ scripts/settingsofdifficutly.js
@@ -15,18 +15,18 @@ Events.on(EventType.ClientLoadEvent, () => {
         CSprompt.cont.add(Core.bundle.get("cruxahh-difficulty-description")).row();
         let buttonLaun = CSprompt.cont.button(Core.bundle.get("cruxahh-difficulty-launchpad")+(Core.settings.getBool("cruxahh-laun")?stringDisabled:stringEnabled),()=>{
             let buf=Core.settings.getBool("cruxahh-laun");
-            if(buf)Blocks.launchPad.buildVisibility=BuildVisibility.editorOnly;
-            else Blocks.launchPad.buildVisibility=BuildVisibility.campaignOnly;
+            if(buf)Blocks.launchPad.buildVisibility=BuildVisibility.campaignOnly;
+            else Blocks.launchPad.buildVisibility=BuildVisibility.editorOnly;
             Core.settings.put("cruxahh-laun",!buf);
-            buttonLaun.setText(Core.bundle.get("cruxahh-difficulty-launchpad")+(Core.settings.getBool("cruxahh-laun")?stringDisabled:stringEnabled));
+            buttonLaun.setText(Core.bundle.get("cruxahh-difficulty-launchpad")+(buf?stringEnabled:stringDisabled));
         });
         buttonLaun.size(600,75).row();
         buttonLaun=buttonLaun.get();
         let buttonWave = CSprompt.cont.button(Core.bundle.get("cruxahh-difficulty-wavesim")+(Core.settings.getBool("cruxahh-wave")?stringDisabled:stringEnabled),()=>{
             let buf=Core.settings.getBool("cruxahh-wave");
-            Planets.serpulo.allowWaveSimulation=!buf;
+            Planets.serpulo.allowWaveSimulation=buf;
             Core.settings.put("cruxahh-wave",!buf);
-            buttonWave.setText(Core.bundle.get("cruxahh-difficulty-wavesim")+(Core.settings.getBool("cruxahh-wave")?stringDisabled:stringEnabled));
+            buttonWave.setText(Core.bundle.get("cruxahh-difficulty-wavesim")+(buf?stringEnabled:stringDisabled));
         });
         buttonWave.size(600,75).row();
         buttonWave=buttonWave.get();
