cad0029eef80fcf555c6dd7e01f9ffa982f44a1c
aaa-chinese <tiantiandd@outlook.com>
2024-08-29 16:54:01 +0800

    the return of the button

--- scripts/settingsofdifficutly.js
+++ scripts/settingsofdifficutly.js
@@ -1,44 +1,43 @@
 Events.on(EventType.ClientLoadEvent, () => {
-    // if(Core.settings.getBool("cruxahh-diff")==true){
-    //     Planets.serpulo.allowWaveSimulation=false;
-    //     Blocks.launchPad.buildVisibility=BuildVisibility.editorOnly;
-    // }else{
-    //     Planets.serpulo.allowWaveSimulation=true;
-    //     Blocks.launchPad.buildVisibility=BuildVisibility.campaignOnly;
-    // }
-    // Vars.ui.menufrag.addButton("@cruxahh-difficulty-button",Icon.logic,() => {
-    //     // let buf = Core.settings.getBool("cruxahh-diff");
-    //     // buf=!buf;
-    //     // Core.settings.put("cruxahh-diff", buf);
-    //     let diffString="";
-    //     Core.settings.getBool("cruxahh-diff")==0?diffString=Core.bundle.get("cruxahh-difficulty-normal")
-    //                                             :diffString=Core.bundle.get("cruxahh-difficulty-hard");
-    //     const CSprompt = new BaseDialog("@cruxahh-difficulty-title");
-    //     CSprompt.cont.add(Core.bundle.get("cruxahh-difficulty-description")+diffString).row();
-    //     CSprompt.cont.button("@cruxahh-difficulty-normal",()=>{
-    //         Core.settings.put("cruxahh-diff",false);
-    //         Planets.serpulo.allowWaveSimulation=true;
-    //         Blocks.launchPad.buildVisibility=BuildVisibility.campaignOnly;
-    //         CSprompt.hide();
-    //     }).size(600,75).row();
-    //     CSprompt.cont.button("@cruxahh-difficulty-hard",()=>{
-    //         Core.settings.put("cruxahh-diff",true);
-    //         Planets.serpulo.allowWaveSimulation=false;
-    //         Blocks.launchPad.buildVisibility=BuildVisibility.editorOnly;
-    //         CSprompt.hide();
-    //     }).size(600,75);
-    //     CSprompt.show();
-    // })
-
-    Vars.ui.settings.addCategory(Core.bundle.get("cruxahh-settings"), "cruxahh-setting-icon", cruxs => {
-        cruxs.checkPref("cruxahh-launchpad", true);
-        cruxs.checkPref("cruxahh-sim", true);
-    })
-
-    if (Core.settings.getBool("cruxahh-launchpad") == true){
+    if(Core.settings.getBool("cruxahh-laun")==true){
         Blocks.launchPad.buildVisibility=BuildVisibility.editorOnly;
     }
-    if (Core.settings.getBool("cruxahh-sim") == true){
+    if(Core.settings.getBool("cruxahh-wave")==true){
         Planets.serpulo.allowWaveSimulation=false;
     }
+    Vars.ui.menufrag.addButton("@cruxahh-difficulty-button",Icon.logic,() => {
+        // let buf = Core.settings.getBool("cruxahh-diff");
+        // buf=!buf;
+        // Core.settings.put("cruxahh-diff", buf);
+        let stringEnabled="@cruxahh-difficulty-enabled";
+        let stringDisabled="@cruxahh-difficulty-disabled"
+        const CSprompt = new BaseDialog("@cruxahh-difficulty-title");
+        CSprompt.cont.add(Core.bundle.get("cruxahh-difficulty-description")).row();
+        CSprompt.cont.button(Core.bundle.get("cruxahh-difficulty-launchpad")+(Core.settings.getBool("cruxahh-laun")?stringDisabled:stringEnabled),()=>{
+            let buf=Core.settings.getBool("cruxahh-laun");
+            if(buf)Blocks.launchPad.buildVisibility=BuildVisibility.editorOnly;
+            else Blocks.launchPad.buildVisibility=BuildVisibility.campaignOnly;
+            Core.settings.put("cruxahh-laun",!buf);
+            CSprompt.hide();
+        }).size(600,75).row();
+        CSprompt.cont.button(Core.bundle.get("cruxahh-difficulty-wavesim")+(Core.settings.getBool("cruxahh-wave")?stringDisabled:stringEnabled),()=>{
+            let buf=Core.settings.getBool("cruxahh-wave");
+            Planets.serpulo.allowWaveSimulation=!buf;
+            Core.settings.put("cruxahh-wave",!buf);
+            CSprompt.hide();
+        }).size(600,75);
+        CSprompt.show();
+    })
+
+    // Vars.ui.settings.addCategory(Core.bundle.get("cruxahh-settings"), "cruxahh-setting-icon", cruxs => {
+    //     cruxs.checkPref("cruxahh-launchpad", true);
+    //     cruxs.checkPref("cruxahh-sim", true);
+    // })
+
+    // if (Core.settings.getBool("cruxahh-launchpad") == true){
+    //     Blocks.launchPad.buildVisibility=BuildVisibility.editorOnly;
+    // }
+    // if (Core.settings.getBool("cruxahh-sim") == true){
+    //     Planets.serpulo.allowWaveSimulation=false;
+    // }
 });
